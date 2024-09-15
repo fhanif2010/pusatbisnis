14d4616fa6cfb3ac807f60a80bc7c2341fc1ce3e
unknown <fhanif665@gmail.com>
2024-09-15 15:59:14 +0700

    menambahkan component product dan service pada home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -34,6 +34,75 @@ const Home = () => {
                         <button>Contact Us</button>
                     </div>
                 </div>
+
+                <div className="home-content">
+                    <div className="home-banner">
+                        <h2>Upcoming Event</h2>
+                        <hr />
+                    </div>
+                    <div className="home-cards">
+                        <div className="home-card" style={{ backgroundColor: 'white', borderRadius: '8px',padding: '10px', textAlign: 'center' }}>
+                            <img src={Wedding} alt="" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
+                            <p>Webinar Barista Series 4</p>
+                        </div>
+                        <div className="home-card" style={{ backgroundColor: 'white', borderRadius: '8px',padding: '10px', textAlign: 'center' }}>
+                            <img src={Wedding} alt="" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
+                            <p>Webinar Barista Series 4</p>
+                        </div>
+                        <div className="home-card" style={{ backgroundColor: 'white', borderRadius: '8px',padding: '10px', textAlign: 'center' }}>
+                            <img src={Wedding} alt="" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
+                            <p>Webinar Barista Series 4</p>
+                        </div>
+                        <div className="home-card" style={{ backgroundColor: 'white', borderRadius: '8px',padding: '10px', textAlign: 'center' }}>
+                            <img src={Wedding} alt="" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
+                            <p>Webinar Barista Series 4</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='home-content'>
+                    <div className='home-content-about'>
+                        <img src={UTCC} alt="" />
+                        <div style={{ marginBottom: '30px' }}>
+                            <div className="home-banner" style={{ marginBottom: '10px' }}>
+                                <h2>About Us</h2>
+                                <hr />
+                            </div>
+                            <h1>Pusat Bisnis Universitas Terbuka</h1>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <a href="">Learn More...</a>
+                        </div>
+                    </div>
+                </div>
+                <div className='home-content' style={{ backgroundColor: '#264653', color: 'white' }}>
+                    <div className="home-banner" style={{ color: 'white' }}>
+                        <h2>What Services Are Available</h2>
+                        <hr />
+                    </div>
+                    <div>
+                        <div className="home-cards">
+                            <div className="home-cardService">
+                                <img src={Wedding} alt="" />
+                                <h2>Wedding or Event</h2>
+                                <p>Lorem Ipsume simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                            </div>
+                            <div className="home-cardService">
+                                <img src={Klinik} alt="" />
+                                <h2>Klinik Pratama</h2>
+                                <p>Lorem Ipsume simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                            </div>
+                            <div className="home-cardService">
+                                <img src={Background} alt="" />
+                                <h2>Wisma</h2>
+                                <p>Lorem Ipsume simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                            </div>
+                            <div className="home-cardService">
+                                <img src={Diklat} alt="" />
+                                <h2>Training Institute</h2>
+                                <p>Lorem Ipsume simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="home-content">
                     <div className="home-banner">
                         <h2>What We Do</h2>
@@ -56,23 +125,6 @@ const Home = () => {
                             <p>Lorem Ipsume simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                         </div>
                     </div>
-                </div>
-                <div className='home-content'>
-                    <div className='home-content-about'>
-                        <img src={UTCC} alt="" />
-                        <div>
-                            <div className="home-banner" style={{ marginBottom: '10px' }}>
-                                <h2>About Us</h2>
-                                <hr />
-                            </div>
-                            <h1>Pusat Bisnis Universitas Terbuka</h1>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            <a href="">Learn More...</a>
-                        </div>
-                    </div>
-                </div>
-                <div>
-
                 </div>
             </div>
         </div>
